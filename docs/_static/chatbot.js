0638e25346083d8c05dcb7b71e8ccb2b7cf65e07
LisaLuice <lisaluice778@gmail.com>
2025-06-25 11:57:37 +0530

    Update chatbot.js

--- docs/_static/chatbot.js
+++ docs/_static/chatbot.js
@@ -1,7 +1,13 @@
 
 document.addEventListener("DOMContentLoaded", function () {
+
     var ze = document.createElement('script');
+
     ze.id = 'ze-snippet';
-    ze.src = 'ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=7668c976-0fb7-47ee-8740-3e9115bdb10f';
+
+    ze.src = 'https://static.zdassets.com/ekr/snippet.js?key=7668c976-0fb7-47ee-8740-3e9115bdb10f';
+
     document.body.appendChild(ze);
+
 });
+ 
